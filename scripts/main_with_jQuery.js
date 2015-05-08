3bbcd309e9566e49b5b4992eea100757d5aa5020
Pedro Martin <pataruco@gmail.com>
2015-05-08 20:03:08 +0100

    soundmanager is playing

--- scripts/main_with_jQuery.js
+++ scripts/main_with_jQuery.js
@@ -4,3 +4,30 @@ function showGreeting (){
   var output = "WDI 13 speaks " + $(this).attr('id');
   $('p').text(output);
 }
+
+
+soundManager.onload = function(){
+  soundManager.createSound()
+}
+
+soundManager.setup({
+  // where to find flash audio SWFs, as needed
+  url: 'audiofiles',
+  onready: function() {
+    var spanish = soundManager.createSound({
+      id:'spanish',
+      url: 'audiofiles/spanish.mp3'
+    })
+    spanish.play();
+  }
+});
+
+// soundManager.setup({
+//   url: '/path/to/swf-files/',
+//   onready: function() {
+//     var mySound = soundManager.createSound({
+//       id: 'aSound',
+//       url: '/path/to/an.mp3'
+//     });
+//     mySound.play();
+//   },
