4f78ed4fb249c69b02b34626862cc22ca67aa11b
Pedro Martin <pataruco@gmail.com>
2015-05-09 09:54:24 +0100

    document.ready working

--- scripts/main_with_jQuery.js
+++ scripts/main_with_jQuery.js
@@ -1,20 +1,22 @@
-soundManager.setup({
-  // where to find flash audio SWFs, as needed
-  url: 'audiofiles',
-  onready: function() {
-    var spanish = soundManager.createSound({
-      id:'spanish',
-      url: 'audiofiles/spanish.mp3'
-    })
-  $('.greeting').on("click", showGreeting);
+$(document).ready(function(){
+  soundManager.setup({
+    // where to find flash audio SWFs, as needed
+    url: 'audiofiles',
+    onready: function() {
+      var spanish = soundManager.createSound({
+        id:'spanish',
+        url: 'audiofiles/spanish.mp3'
+      })
+    $('.greeting').on("click", spanish.play());
 
-  function showGreeting (){
-    var output = "WDI 13 speaks " + $(this).attr('id');
-    $('p').text(output);
-  }
+    function showGreeting (){
+      var output = "WDI 13 speaks " + $(this).attr('id');
+      $('p').text(output);
+    }
 
 
-    spanish.play();
-  }
-});
+      spanish.play();
+    }
+  }); //soundmanager
 
+}) // document.ready
